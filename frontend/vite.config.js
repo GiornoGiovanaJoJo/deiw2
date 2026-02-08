7fa8849fb8f993669c5c6ef3fdfee08ac2c6a9bb
GiornoGiovanaJoJo <kbootovsk@gmail.com>
2026-02-08 19:06:08 +0300

    fix: simplify vite alias path

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "src"),
     },
     extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json'],
   },
